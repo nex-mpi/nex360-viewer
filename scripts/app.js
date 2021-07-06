2f471bddc51f2b2dbca7ed6844d004439f3df2e8
Pakkapon Phongthawee <me@pureapp.in.th>
2021-07-07 01:40:47 +0700

    weight working

--- scripts/app.js
+++ scripts/app.js
@@ -168,7 +168,6 @@ void main(void)
 }
 `
 
-
 class NeXviewerApp{
     constructor(cfg, p_bary_ids, p_bary_weights, p_bary_height, p_bary_width, callback){
         this.cfg = cfg
@@ -199,11 +198,20 @@ class NeXviewerApp{
         var ratio = window.innerWidth / window.innerHeight
         this.camera = new THREE.PerspectiveCamera(this.cfg.fov_degree, ratio, 0.1, 1000 );
         this.camera.up.set( 0, 0, 1 );
-        this.renderer = new THREE.WebGLRenderer({ alpha: true }); 
+        this.renderer = new THREE.WebGLRenderer({ alpha: true, preserveDrawingBuffer: true}); 
         this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement );
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.renderer.setClearColor( 0xffffff, 1 ); //change to white background
         document.body.appendChild(this.renderer.domElement );       
+        this.composer = new THREE.EffectComposer(this.renderer);
+        //this.clearPass = new THREE.ClearPass(0xffffff,1);
+        //this.composer.addPass(this.clearPass);
+        this.renderPass = new THREE.RenderPass(this.scene, this.camera);
+        //this.renderPass.clear = false;
+        this.composer.addPass(this.renderPass);
+        this.opacityPass = new THREE.ShaderPass(THREE.CopyShader);
+        this.opacityPass.material.transparent = true;
+        this.composer.addPass(this.opacityPass);
     }
     initScene(){       
         // prepare scene     
@@ -219,7 +227,8 @@ class NeXviewerApp{
         this.camera.position.z = 2.066666603088379;
         this.materials = {};
         this.mpis = {};
-        this.mpis['first_mpi_id'] = 0;
+        this.mpis_ids = [0,1,2];
+        this.prev_id = 0;
         var fov_tan = Math.tan(this.cfg.fov_radian / 2.0);
         for(var mpiId = 0; mpiId < 30; mpiId++) //this.cfg.c2ws.length
         {
@@ -311,12 +320,12 @@ class NeXviewerApp{
     }
     animate(){
         this.stats.begin();
-        this.renderer.render(this.scene, this.camera );
+        //this.renderer.render(this.scene, this.camera );
         this.controls.update();
         /////       
         this.write_camera_location();
         var bary = this.bary();     
-           
+        /*  
         var t = 2;
         if(bary['weights'][0] >= bary['weights'][1] && bary['weights'][0] >= bary['weights'][2]){
             t = 0;
@@ -324,35 +333,59 @@ class NeXviewerApp{
         if(bary['weights'][1] >= bary['weights'][0] && bary['weights'][1] >= bary['weights'][2]){
             t = 1;
         }
-        var id = bary['ids'][t];
+        */
         localStorage.setItem('bary_address',JSON.stringify([id]));
+        //this.renderer.clear();
+        this.renderer.autoClear = true;
+        //this.clearPass.enabled = true;
+        for(var b = 0; b < 3; b++){
+            var id = bary['ids'][b];
+            var weight = bary['weights'][b];
+            this.mpis[b].group.visible = true;
+            if(this.mpis_ids[b] != id) {
+                this.mpis[b].group.applyMatrix4(this.matrices['w2cs'][this.mpis_ids[b]]);
+                this.mpis[b].group.applyMatrix4(this.matrices['c2ws'][id]);
+                for(var planeId = 0; planeId < this.cfg.planes.length; planeId++){
+                    this.mpis[b].planes[planeId].material = this.materials[id][planeId];
+                }
+                this.mpis_ids[b]= id;            
+            }
+            this.opacityPass.uniforms.opacity.value = weight;
+            //this.renderer.render(this.scene, this.camera );
+            this.renderer.autoClear = false;
+            this.composer.render();
+            //this.clearPass.enabled = false;
+            this.mpis[b].group.visible = false;
+        }
+        
+        /*
         if(id != this.mpis['first_mpi_id']){
-            /*
-            var old_id = this.mpis['first_mpi_id'];
-            this.mpis[old_id].group.visible = false;
-            this.mpis[id].group.visible = true;           
-            this.mpis['first_mpi_id'] = id;
-            */
+            //var old_id = this.mpis['first_mpi_id'];
+            //this.mpis[old_id].group.visible = false;
+            //this.mpis[id].group.visible = true;           
+            //this.mpis['first_mpi_id'] = id;
+            this.opacitypass.uniforms.opacity.value = 0.2;
             var old_id = this.mpis['first_mpi_id'];
             this.mpis[0].group.applyMatrix4(this.matrices['w2cs'][old_id]);
             this.mpis[0].group.applyMatrix4(this.matrices['c2ws'][id]);
             for(var planeId = 0; planeId < this.cfg.planes.length; planeId++){
                 this.mpis[0].planes[planeId].material = this.materials[id][planeId];
-                /*
-                this.mpis[0].planes[planeId].material.uniforms.mpi_a.value = this.textures[id].a;
-                this.mpis[0].planes[planeId].material.uniforms.mpi_c.value = this.textures[id].c;
-                this.mpis[0].planes[planeId].material.uniforms.mpi_coeff.value = this.textures[id].coeff;
-                */
+                //this.mpis[0].planes[planeId].material.uniforms.mpi_a.value = this.textures[id].a;
+                //this.mpis[0].planes[planeId].material.uniforms.mpi_c.value = this.textures[id].c;
+                //this.mpis[0].planes[planeId].material.uniforms.mpi_coeff.value = this.textures[id].coeff;
             }
             this.mpis['first_mpi_id'] = id;            
-        }       
+        }*/       
         
         ///////
         this.stats.end();
         requestAnimationFrame(this.render.bind(this));
     }
     cleanupPrecompile(){
-        for(var i = 1; i < 30; i++){
+        for(var i = 0; i < 3; i++){
+            this.mpis[i].group.visible = false;
+        }
+        for(var i = 3; i < 30; i++){
             this.mpis[i].group.clear();
             this.mpis[i].group.removeFromParent();
         }
