bb34f3f42f1d7f760cfb1baa6a8d96716a106da0
Pakkapon Phongthawee <me@pureapp.in.th>
2021-07-12 13:34:55 +0700

    replace previous week renderer with nano

--- experiment/texture16/scripts/blendFragmentShader.js
+++
@@ -1,7 +0,0 @@
-var blendFragmentShader = `
-uniform sampler2D bloomTexture;
-void main() {
-	vUv = uv;
-	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-}
-`;
--- experiment/texture16/scripts/blendVertexShader.js
+++
@@ -1,7 +0,0 @@
-var blendVertexShader = `
-varying vec2 vUv;
-void main() {
-	vUv = uv;
-	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-}
-`;
